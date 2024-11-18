f7c0df2bb0f40dd0249e71e73b42ed541e83b1c9
Mark <astermark66@gmail.com>
2024-11-18 11:02:16 +0200

    updated IntakeTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTest.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "intakeTest" , group = "Tests")
@@ -9,6 +11,8 @@ public class IntakeTest extends LinearOpMode {
    Servo servoGheara;
    Servo servoGhearaDreapta;
    Servo servoRotit;
+   ColorSensor colorSensor;
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry.addData("status: " , "initialized");
@@ -22,25 +26,25 @@ public class IntakeTest extends LinearOpMode {
 
     public void update() {
 
-        while(opModeIsActive()){
-            if(gamepad1.b) {
+        while(opModeIsActive()) {
+            telemetry.addData("RED" , colorSensor.red());
+            telemetry.addData("BLUE" , colorSensor.blue());
+            telemetry.addData("GREEN" , colorSensor.green());
+
+            if(colorSensor.red() > 0.5 && colorSensor.blue() < 0.3 && colorSensor.green() < 0.3) {
                 servoGheara.setPosition(0.7);
-            }
-            else {
-                servoGheara.setPosition(0);
-            }
-            if(gamepad1.a) {
-                servoGhearaDreapta.setPosition(0.7);
-            }
-            else {
-                servoGhearaDreapta.setPosition(0);
-            }
-            if (gamepad1.x){
                 servoRotit.setPosition(0.7);
             }
-            else {
-                servoRotit.setPosition(0);
+            if(colorSensor.blue() > 0.5 && colorSensor.red() < 0.3 && colorSensor.green() < 0.3) {
+                servoGheara.setPosition(0.3);
+                servoRotit.setPosition(0.3);
+
             }
+            else {
+                servoGheara.setPosition(0.5);
+                servoRotit.setPosition(0.5);
+            }
+
         }
     }
 
